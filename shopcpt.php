9fc31b9ff1614ad1c032395c075aa0e11ef2b512
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2021-12-22 21:01:01 +0530

    comments

--- shopcpt.php
+++ shopcpt.php
@@ -1,46 +1,64 @@
 <?php
 /**
- *  @package ShopCPT
+ * @package  Shop CPT Plugin
  */
+/*
+Plugin Name: Shop CPT Plugin
+Plugin URI: https://www.linkedin.com/in/pooja-paul/
+Description: This is a plugin which contains cool functionalities
+Version: 1.0.0
+Author: Pooja Paul
+Author URI: https://www.linkedin.com/in/pooja-paul/
+License: GPLv2 or later
+Text Domain: alecaddd-plugin
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
+*/
+
+// If this file is called firectly, abort!!!
+defined( 'ABSPATH' ) or die( 'You can not Access this file!!' );
+
+// Require once the Composer Autoload
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
 
 /**
- * Plugin Name:       Shop CPT Plugin
- * Plugin URI:        https://donotexist/shopcpt/
- * Description:       Will create a CPT named Shop and have additional custom fields.
- * Version:           1.0
- * Requires at least: 5.2
- * Requires PHP:      7.2
- * Author:            Pooja Paul
- * Author URI:        https://donotexist.com/
- * License:           GPL v2 or later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://donotexist.com/shopcpt/
- * Text Domain:       shop-cpt
+ * The code that runs during plugin activation
  */
-
-defined('ABSPATH') or die('You can not Access this file!!');
-
-if (file_exists(dirname(__FILE__).'/vendor/autoload.php')) {
-    require_once dirname(__FILE__).'/vendor/autoload.php'; 
+function activate_shop_cpt() {
+	Inc\Base\Activate::activate();
 }
+register_activation_hook( __FILE__, 'activate_shop_cpt' );
 
-
-function activate_shop_cpt() 
-{
-    Inc\Base\Activate::activate(); 
+/**
+ * The code that runs during plugin deactivation
+ */
+function deactivate_shop_cpt() {
+	Inc\Base\Deactivate::deactivate();
 }
-register_activation_hook(__FILE__ ,'activate_shop_cpt');
+register_deactivation_hook( __FILE__, 'deactivate_shop_cpt' );
 
-
-function deactivate_shop_cpt()
-{
-    Inc\Base\deactivate::deactivate(); 
+/**
+ * Initialize all the core classes of the plugin
+ */
+if ( class_exists( 'Inc\\Init' ) ) {
+	Inc\Init::register_services();
 }
-register_deactivation_hook(__FILE__ ,'deactivate_shop_cpt');
-
-
-if (class_exists('Inc\\Init')) {
-     Inc\Init::register_services();
-}
-
 
